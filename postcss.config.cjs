27b395747f14dce210e54cc617c9b6a1469cddf0
Wansiri-D <112197738+Wansiri-D@users.noreply.github.com>
2025-05-17 18:54:24 +0200

    Updates

--- postcss.config.cjs
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  plugins: {
-    '@tailwindcss/postcss': {}, // เปลี่ยนจาก tailwindcss เป็น @tailwindcss/postcss
-    autoprefixer: {},
-  },
-};
--- src/pages/Order.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import OrderConfirm from '../components/OrderConfirm.jsx';
-
-const Order = () => {
-  return <OrderConfirm />;
-};
-
-export default Order;
